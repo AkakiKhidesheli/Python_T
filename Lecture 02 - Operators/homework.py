c739554d8631b151acee12c54388d37b5cc3e98f
AkakiKhidesheli <khidesheli.akaki@gmail.com>
2025-02-06 22:58:25 +0400

    first commit

--- Lecture 02 - Operators/homework.py
+++ Lecture 02 - Operators/homework.py
@@ -1,9 +1,9 @@
-# # დავალება 1
-# number1=int(input("Enter a number: "))
-# if number1%2==1:
-#     print("Odd")
-# else:
-#     print("Even")
+# დავალება 1
+number1 = int(input("Enter a number: "))
+if number1 % 2 == 1:
+    print("Odd")
+else:
+    print("Even")
 
 # დავალება 2
 text1 = input("Enter a text: ")
@@ -18,3 +18,22 @@ if 'middle' in text1:
 
 if 'small' not in text1 and 'tall' not in text1 and 'middle' not in text1:
     print('small, tall and middle not in text')
+
+# დავალება 3
+a = int(input("შეიყვანეთ მთელი რიცხვი a: "))
+b = int(input("შეიყვანეთ მთელი რიცხვი b: "))
+operator = input('შეიყვანეთ მათემატიკური ოპერატორი: ')
+if operator == '+':
+    print(f'a+b={a + b}')
+elif operator == '-':
+    print(f'a-b={a - b}')
+elif operator == '*':
+    print(f'a*b={a * b}')
+elif operator == '/':
+    print(f'a/b={a / b}')
+elif operator == '//':
+    print(f'a-ს b-ზე გაყოფისას მთელი ნაწილია {a // b}')
+elif operator == '%':
+    print(f'a-ს b-ზე გაყოფისას ნაშთია {a % b}')
+elif operator == '**':
+    print(f'a^b={a ** b}')
